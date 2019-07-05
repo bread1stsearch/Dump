d41a1d8d4e96591273296f970fd53e12440095f7
Chloe Duan <chloe.duan@hotmail.com>
2019-07-05 00:08:46 -0700

    Delete setup methods in models

--- topics/models.py
+++ topics/models.py
@@ -9,10 +9,6 @@ class Topic(models.Model):
     add_date = models.DateTimeField('date added')
     is_acronym = models.BooleanField(default=False)
 
-    def setup(self, topic_text="default topic title"):
-        self.topic_text = topic_text
-        self.add_date = timezone.now()
-
     def was_added_recently(self):
         return self.add_date >= timezone.now() - datetime.timedelta(days=7)
 
@@ -32,11 +28,6 @@ class Oneliner(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.CharField(max_length=1000)
     last_update = models.DateTimeField('last updated')
-
-    def setup(self, topic, text="default oneliner"):
-        self.topic = topic
-        self.text = text
-        self.last_update = timezone.now()
 
     def __str__(self):
         return "Oneliner for topic {}: {}".format(self.topic.topic_text, self.text)
