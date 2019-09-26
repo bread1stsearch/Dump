1da51fbd56f565607bfe355acdd0e5b585f43229
Chloe Duan <chloe.duan@hotmail.com>
2019-09-25 21:17:13 -0700

    reformat code

--- Dump/frontend/src/components/Topics.js
+++ Dump/frontend/src/components/Topics.js
@@ -10,131 +10,135 @@ import 'babel-polyfill';
 class Topics extends React.Component {
   constructor(props)
   {
-    super(props);
-    this.state =
-    {
-        display:true,
-        topics:[],
-    }
+	super(props);
+	this.state =
+	{
+		display:true,
+		topics:[],
+	}
   }
 
   componentDidUpdate(prevState)
   {
-    console.log("componentDidUpdate");
-    if (prevState.topics != this.state.topics)
-    {
-        console.log("topic has updated");
-    }
+	console.log("componentDidUpdate");
+	if (prevState.topics != this.state.topics)
+	{
+		console.log("topic has updated");
+	}
   }
 
   getTopics()
   {
-    console.log('getTopics');
+	console.log('getTopics');
 
-    fetch(this.props.endpoint)
-        .then(res => res.json())
-        .then((data) => {
-          this.setState({ topics: data }, () => {
-          console.log("SET STATE FOR TOPIC AFTER GET" + JSON.stringify(this.state.topics))
-          })
-        })
-        .catch(console.log);
-    //this.forceUpdate();
+	fetch(this.props.endpoint)
+		.then(res => res.json())
+		.then((data) => {
+		  this.setState({ topics: data }, () => {
+		  console.log("SET STATE FOR TOPIC AFTER GET" + JSON.stringify(this.state.topics))
+		  })
+		})
+		.catch(console.log);
+	//this.forceUpdate();
   }
 
   componentDidMount()
   {
-    console.log('componentDidMount');
-    this.getTopics();
+	console.log('componentDidMount');
+	this.getTopics();
   }
 
   redirectTopic({match})
   {
-    return (
-        <Topic endpoint={`/topics/serialize/${match.params.id}`} />
-    );
+	return (
+		<Topic endpoint={`/topics/serialize/${match.params.id}`} />
+	);
   }
 
   async deleteTopic(id)
   {
-        console.log('deleteTopic');
+		console.log('deleteTopic');
 
-        var csrftoken = Cookies.get('csrftoken');
+		var csrftoken = Cookies.get('csrftoken');
 
-        // TODO: how to make requests with '/' at the end synonymous with non-suffixed '/'
-        await fetch("topics/serialize/"+id+"/", {method:"DELETE", headers:{'X-CSRFToken': csrftoken}}).catch(console.log);
-        this.getTopics();
+		// TODO: how to make requests with '/' at the end synonymous with non-suffixed '/'
+		await fetch("topics/serialize/"+id+"/", {method:"DELETE", headers:{'X-CSRFToken': csrftoken}}).catch(console.log);
+		this.getTopics();
+  }
+
+  async postTopic()
+  {
   }
 
   editTopic()
   {
-
+	return (<div><h1> edit </h1></div>);
   }
 
   renderTopics()
   {
     console.log('renderTopics');
-    const topics = this.state.topics.map(
-        function (topic) {
-            return (
-                <li key={uniqueId()} className="topicBlob" >
-                    <Router>
-                        <a href={`topics/${topic.id}/`}>
-                            { topic.topic_text }{ JSON.parse(topic.is_acronym) ? ` - ${topic.acronym.acronym_expanded}` : ""}
-                        </a>
-                        <Route path={`/${topic.id}/`} render={
-                                                    (props) => redirectTopic(props.match)
-                                                    } />
-                    </Router>
-                    <button onClick={() => this.editTopic()}> Edit </button>
-                    <button onClick={() => this.deleteTopic(topic.id)}> Delete </button>
-                </li>
+	const topics = this.state.topics.map(
+		function (topic) {
+			return (
+				<li key={uniqueId()} className="topicBlob" >
+					<Router>
+						<a href={`topics/${topic.id}/`}>
+							{ topic.topic_text }{ JSON.parse(topic.is_acronym) ? ` - ${topic.acronym.acronym_expanded}` : ""}
+						</a>
+						<Route path={`/${topic.id}/`} render={
+													(props) => redirectTopic(props.match)
+													} />
+					</Router>
+					<button onClick={() => this.editTopic()}> Edit </button>
+					<button onClick={() => this.deleteTopic(topic.id)}> Delete </button>
+				</li>
 
-                    );
-        },this)
+					);
+		},this)
 
 
-    if (this.state.display)
-    {
-        return (
-            <div>
-                <p> {this.state.topics.length} </p>
+	if (this.state.display)
+	{
+		return (
+			<div>
+				<p> {this.state.topics.length} </p>
 
-                <ReactSortable tag="ul"> {topics} </ReactSortable>
+				<ReactSortable tag="ul"> {topics} </ReactSortable>
 
-            </div>
-        );
-    }
-    else
-    {
-        return (<p> No topics </p>);
-    }
+			</div>
+		);
+	}
+	else
+	{
+		return (<p> No topics </p>);
+	}
   }
 
   handleClick()
   {
-    this.setState({display:!this.state.display});
+	this.setState({display:!this.state.display});
   }
 
   render() {
-    console.log('render');
+	console.log('render');
 
-    return (
-    <div>
-        <p> {this.props.endpoint} </p>
+	return (
+	<div>
+		<p> {this.props.endpoint} </p>
 
-        <button onClick={() => this.handleClick()}>
-            Toggle Display
-        </button>
+		<button onClick={() => this.handleClick()}>
+			Toggle Display
+		</button>
 
-      <h1> topics </h1>
-      <p> {JSON.stringify(this.state.topics)} </p>
-      <div>
-      {this.renderTopics()}
-      </div>
+	  <h1> topics </h1>
+	  <p> {JSON.stringify(this.state.topics)} </p>
+	  <div>
+	  {this.renderTopics()}
+	  </div>
 
-    </div>
-    );
+	</div>
+	);
 
   }
 }
