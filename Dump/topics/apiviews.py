d857193aa2ef58b736517e5b9436d3fb1f569e10
Chloe Duan <chloe.duan@hotmail.com>
2019-10-02 00:27:21 -0700

    POST topic through API pipeline flows starting here

--- Dump/topics/apiviews.py
+++ Dump/topics/apiviews.py
@@ -1,8 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
 from topics.models import Topic, Oneliner, Acronym
 from topics.serializers import TopicSerializer, OnelinerSerializer, AcronymSerializer
+from django.http import JsonResponse
 
 class TopicList(generics.ListCreateAPIView):
     queryset = Topic.objects.all()
@@ -24,6 +26,30 @@ class TopicList(generics.ListCreateAPIView):
         to_delete.delete()
         return Response("204")
 
+    def post(self, request):
+        data = JSONParser().parse(request)
+
+        serializer = TopicSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+        # acronym = None
+        # if acronym is not None:
+        #     if topic.is_valid() and acronym.is_valid():
+        #         topic.save()
+        #         acronym.save()
+        #         return Response("200")
+        #     elif not topic.is_valid():
+        #         return Response(topic.errors, status=status.HTTP_400_BAD_REQUEST)
+        #     elif not acronym.is_valid():
+        #         return Response(acronym.errors, status=status.HTTP_400_BAD_REQUEST)
+        # else:
+        #     if topic.is_valid():
+        #         topic.save()
+        #         return Response("200")
+        #     else:
+        #         return Response(topic.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TopicDetail(generics.RetrieveUpdateDestroyAPIView):
--- Dump/topics/serializers.py
+++ Dump/topics/serializers.py
@@ -3,6 +3,8 @@ from rest_framework.renderers import JSONRenderer
 from topics.models import Topic, Oneliner, Acronym
 from rest_framework.response import Response
 from django.db import models
+from django.utils import timezone
+
 
 # NOTE: Nested Serialization
 # https://stackoverflow.com/questions/29651945/django-rest-framework-nested-serializer-not-showing-related-data/29652574
@@ -22,11 +24,39 @@ class AcronymSerializer(serializers.ModelSerializer):
 class TopicSerializer(serializers.ModelSerializer):
     # if many to one relationship,
     # <fieldname>_set references list of objects with foreign key to topic
-    oneliner_set = OnelinerSerializer(many=True, read_only=True)
-    acronym = AcronymSerializer(many=False, read_only=True)
+    topic_text = serializers.CharField(max_length=200)
+    is_acronym = serializers.BooleanField(default=False)
+    oneliner_set = OnelinerSerializer(many=True, required=False, allow_null=True)
+    acronym = AcronymSerializer(many=False, required=False, allow_null=True)
 
     class Meta:
         model = Topic
         fields = ['id', 'topic_text', 'is_acronym', 'oneliner_set', 'acronym']
         depth = 1
 
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        # acronym_expanded = validated_data.pop('acronym_expanded')
+        # oneliners = validated_data.pop('oneliner_set')
+        # topic = Topic.objects.create(**validated_data)
+        # for o in oneliners:
+        #     topic = Topic.objects.get(pk=topic.pk)
+        #     topic.oneliner_set.create(topic_id=topic.pk,
+        #                                         text=o,
+        #                                         last_update=timezone.now())
+        # Acronym.objects.create(topic=topic, acronym_expanded=acronym_expanded)
+        if validated_data["is_acronym"]:
+            acronym_expanded = validated_data.pop('acronym_expanded')
+        topic = Topic.objects.create(**validated_data)
+        return topic
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.topic_text = validated_data.get('topic_text', instance.topic_text)
+        instance.is_acronym = validated_data.get('is_acronym', instance.is_acronym)
+        instance.save()
+        return instance
