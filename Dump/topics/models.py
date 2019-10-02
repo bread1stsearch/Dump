55bd6e753ef3663093dbfc4bfbd3ff6c5344293b
Chloe Duan <chloe.duan@hotmail.com>
2019-10-02 00:25:10 -0700

    Rename Topic add_date field to created. Re-stringify Acronym to use topic.topic_text

--- Dump/topics/models.py
+++ Dump/topics/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 # Create your models here.
 class Topic(models.Model):
     topic_text = models.CharField(max_length=200)
-    add_date = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
     is_acronym = models.BooleanField(default=False)
 
     def get_absolute_url(self):
@@ -14,21 +14,21 @@ class Topic(models.Model):
 
     def setup(self, topic_text="default topic title"):
         self.topic_text = topic_text
-        self.add_date = timezone.now()
+        self.created = timezone.now()
 
     def was_added_recently(self):
-        return self.add_date >= timezone.now() - datetime.timedelta(days=7)
+        return self.created >= timezone.now() - datetime.timedelta(days=7)
 
     def __str__(self):
         return self.topic_text
 
 
 class Acronym(models.Model):
-    topic = models.OneToOneField(Topic, on_delete=models.CASCADE)
+    topic = models.OneToOneField(Topic, on_delete=models.CASCADE, null=True)
     acronym_expanded = models.CharField(max_length=500)
 
     def __str__(self):
-        return "{} - {}".format(self.topic, self.acronym_expanded)
+        return "{} - {}".format(self.topic.topic_text, self.acronym_expanded)
 
 
 class Oneliner(models.Model):
